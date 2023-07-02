65bd43650ee0d29bc92d2a5ff57a9c8eb08403d9
Jean70402 <108194427+Jean70402@users.noreply.github.com>
2023-07-02 14:29:27 -0500

    Add files via upload

---
+++ src/hidraulicagrupo/HidraulicaGrupo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hidraulicagrupo;
+
+/**
+ *
+ * @author jean7
+ */
+public class HidraulicaGrupo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/hidraulicagrupo/ProyectoHidraulica.java
@@ -0,0 +1,13 @@
+package hidraulicagrupo;
+
+/**
+ *
+ * @author jean7
+ */
+public class ProyectoHidraulica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
